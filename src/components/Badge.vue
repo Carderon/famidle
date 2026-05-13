dea6c49d94b72bb804ac10fbc28814eb712966e2
Carderon <cedric.l.gillis@gmail.com>
2026-05-13 09:49:25 +0200

    move era badge

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -9,14 +9,14 @@ import { computed } from 'vue';
 import { useCharacterStore } from '@/stores/characterStore.ts'
 
 const badgeStages = [
-  '../../src/assets/default.png',
-  '../../src/assets/firecamp.gif', // https://www.deviantart.com/camilaxiao/art/Warm-Bonfire-Pixel-Art-Animation-856983081
-  'stage2.png',
-  'stage3.png',
-  'stage4.png',
-  'stage5.png',
-  'stage6.png',
-  'stage7.png',
+  '/famidle/default.png',
+  '/famidle/firecamp.gif', // https://www.deviantart.com/camilaxiao/art/Warm-Bonfire-Pixel-Art-Animation-856983081
+  '/famidle/stage2.png',
+  '/famidle/stage3.png',
+  '/famidle/stage4.png',
+  '/famidle/stage5.png',
+  '/famidle/stage6.png',
+  '/famidle/stage7.png',
 ];
 
 const characterStore = useCharacterStore();
